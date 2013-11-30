145c2c2c93b33941c35bf4237e845e6e85be3d9d
glewarne <g.lewarne@gmx.co.uk>
2013-11-30 22:09:25 +0000

    Update ramdisk

--- ramdisk/init.stockmod.sh
+++ ramdisk/init.stockmod.sh
@@ -5,19 +5,19 @@
 
 #re-set what no doubt has been changed by a rom script to more sensible values
 echo 256 > /sys/block/mmcblk0/bdi/read_ahead_kb
-echo 800 > /proc/sys/vm/dirty_expire_centisecs
-echo 60 > /proc/sys/vm/dirty_ratio
-echo 40 > /proc/sys/vm/dirty_background_ratio
-echo 30 > /proc/sys/vm/vfs_cache_pressure
+#echo 800 > /proc/sys/vm/dirty_expire_centisecs
+#echo 60 > /proc/sys/vm/dirty_ratio
+#echo 40 > /proc/sys/vm/dirty_background_ratio
+#echo 30 > /proc/sys/vm/vfs_cache_pressure
 
 #set simple GPU governor as default, added by StockMOD kernel
 # echo simple > /sys/devices/fdb00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/pwrscale/trustzone/governor
 
 #ondemand governor tweaks, performance/battery balance
-echo 20000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
-echo 60 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
-echo 60 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
-echo 60 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
+echo 15000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+echo 70 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+#echo 50 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core
+#echo 50 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
 
 
 #we can set our own cpu sync frequencies below
